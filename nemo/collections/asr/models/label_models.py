68b2a38da787ae5afec5770c1fbfbb735d9be43e
bigbang123789789 <134249895+bigbang123789789@users.noreply.github.com>
2024-03-20 16:08:23 +0700

    Update label_models.py
    Signed-off-by: bigbang123789789 <134249895+bigbang123789789@users.noreply.github.com>

--- nemo/collections/asr/models/label_models.py
+++ nemo/collections/asr/models/label_models.py
@@ -419,7 +419,8 @@ class EncDecSpeakerLabelModel(ModelPT, ExportableEncDecModel):
         self.log(f'{tag}_acc_macro', macro_accuracy_score, sync_dist=True)
         print(f'{tag}_loss : ', loss_mean)
         print(f'{tag}_acc_macro :', macro_accuracy_score)
-
+        torch.save(self.state_dict(),'/content/drive/MyDrive/titanet.pt')
+        
         return {
             f'{tag}_loss': loss_mean,
             f'{tag}_acc_micro_top_k': topk_scores,
